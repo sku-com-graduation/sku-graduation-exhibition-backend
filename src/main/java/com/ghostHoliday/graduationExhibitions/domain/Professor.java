c4173a314ba972cfc306dbd6796850c6b5a5e10b
Yithian01 <Yithian01@git.com>
2025-01-04 18:58:58 +0900

    2024_01_04_3_Entity_create

---
+++ src/main/java/com/ghostHoliday/graduationExhibitions/domain/Professor.java
@@ -0,0 +1,20 @@
+package com.ghostHoliday.graduationExhibitions.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Professor {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String url;
+    private boolean tenure;
+
+}
---
+++ src/main/java/com/ghostHoliday/graduationExhibitions/domain/Role.java
@@ -0,0 +1,6 @@
+package com.ghostHoliday.graduationExhibitions.domain;
+
+
+public enum Role {
+    LEADER, MEMBER
+}
