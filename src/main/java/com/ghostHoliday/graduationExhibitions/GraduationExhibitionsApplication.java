631308113a2015c46e1d14bbb7245dd74551715a
Yithian01 <Yithian01@git.com>
2025-01-04 18:29:44 +0900

    2024_01_04_first_commit

---
+++ src/main/java/com/ghostHoliday/graduationExhibitions/GraduationExhibitionsApplication.java
@@ -0,0 +1,13 @@
+package com.ghostHoliday.graduationExhibitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationExhibitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationExhibitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghostHoliday/graduationExhibitions/GraduationExhibitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghostHoliday.graduationExhibitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationExhibitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
