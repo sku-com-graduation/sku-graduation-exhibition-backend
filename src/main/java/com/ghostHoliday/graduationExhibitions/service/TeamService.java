1a6388e15c0a34ad035f3548dbff18eaa3552ee9
juns0720 <juns0720@naver.com>
2025-01-25 00:50:33 +0900

    feat: implement save and findById

---
+++ src/main/java/com/ghostHoliday/graduationExhibitions/service/TeamService.java
@@ -0,0 +1,22 @@
+package com.ghostHoliday.graduationExhibitions.service;
+
+import com.ghostHoliday.graduationExhibitions.domain.Team;
+import com.ghostHoliday.graduationExhibitions.repository.TeamRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class TeamService {
+    private final TeamRepository teamRepository;
+
+    public Long save(Team team){
+        return teamRepository.save(team).getId();
+    }
+
+    public Team findById(Long teamId){
+        return teamRepository.findById(teamId).get();
+    }
+}
