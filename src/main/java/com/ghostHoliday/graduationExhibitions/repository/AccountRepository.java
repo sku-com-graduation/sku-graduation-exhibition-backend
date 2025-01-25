004df040e93e7e159cbc86e15a46e0fe5a5969b2
juns0720 <juns0720@naver.com>
2025-01-25 12:24:27 +0900

    implement AccountRepository

---
+++ src/main/java/com/ghostHoliday/graduationExhibitions/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.ghostHoliday.graduationExhibitions.repository;
+
+
+import com.ghostHoliday.graduationExhibitions.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    
+}
