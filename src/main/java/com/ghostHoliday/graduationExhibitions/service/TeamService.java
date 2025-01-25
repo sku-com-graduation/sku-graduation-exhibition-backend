925f864482ee51e51b6593715f709ed473da3e23
juns0720 <juns0720@naver.com>
2025-01-26 02:11:34 +0900

    feat: implement updateTeamInfo

--- src/main/java/com/ghostHoliday/graduationExhibitions/service/TeamService.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/service/TeamService.java
@@ -1,8 +1,12 @@
 package com.ghostHoliday.graduationExhibitions.service;
 
+import com.ghostHoliday.graduationExhibitions.domain.Category;
 import com.ghostHoliday.graduationExhibitions.domain.Post;
+import com.ghostHoliday.graduationExhibitions.domain.Professor;
 import com.ghostHoliday.graduationExhibitions.domain.Team;
 import com.ghostHoliday.graduationExhibitions.dto.FindTeamPostInfoByYearDTO;
+import com.ghostHoliday.graduationExhibitions.dto.ResponseTeamInfoDTO;
+import com.ghostHoliday.graduationExhibitions.dto.UpdateTeamInfoDTO;
 import com.ghostHoliday.graduationExhibitions.repository.AccountRepository;
 import com.ghostHoliday.graduationExhibitions.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
@@ -62,7 +66,34 @@ public class TeamService {
         }
     }
 
+    @Transactional
+    public List<ResponseTeamInfoDTO> updateTeamInfo(List<UpdateTeamInfoDTO> updateTeamInfoDTOS) throws Exception {
+        List<ResponseTeamInfoDTO> teams = new ArrayList<>();
+        for (UpdateTeamInfoDTO updateTeamInfoDTO : updateTeamInfoDTOS) {
+            Long teamId = encryptionService.decryptPrimaryKey(updateTeamInfoDTO.getEncryptedTeamId());
+            Team team = teamRepository.findById(teamId).get();
+            ResponseTeamInfoDTO teamInfo = updateTeam(team, teamId, null,
+                    updateTeamInfoDTO.getName(),
+                    updateTeamInfoDTO.getExhibitionYear(),
+                    updateTeamInfoDTO.getCategory());
+            teams.add(teamInfo);
+        }
+        return teams;
+    }
 
+    static ResponseTeamInfoDTO updateTeam(Team team, Long teamId, Long professorId, String name, int year, Category category){
+        ResponseTeamInfoDTO teamInfo = new ResponseTeamInfoDTO();
+
+        team.setProfessor(null);
+        teamInfo.setProfessor(null);
+        team.setName(name);
+        teamInfo.setName(name);
+        team.setExhibitionYear(year);
+        teamInfo.setCategory(category);
+        team.setCategory(category);
+        teamInfo.setCategory(category);
+        return teamInfo;
+    }
 
 
 
