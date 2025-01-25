60225e0a06010f12e01ce9672923fa742ec185d8
juns0720 <juns0720@naver.com>
2025-01-26 00:30:59 +0900

    feat: implement AESUtil

---
+++ src/main/java/com/ghostHoliday/graduationExhibitions/AESUtil.java
@@ -0,0 +1,51 @@
+package com.ghostHoliday.graduationExhibitions;
+
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class AESUtil {
+
+    private static final String ALGORITHM = "AES";
+    private static final int KEY_SIZE = 128;
+    private static final String TRANSFORMATION = "AES";
+
+    // AES 키 생성
+    public static SecretKey generateKey() throws Exception {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
+        keyGenerator.init(KEY_SIZE);
+        return keyGenerator.generateKey();
+    }
+
+    // AES 키를 Base64 문자열로 변환
+    public static String encodeKey(SecretKey secretKey) {
+        return Base64.getEncoder().encodeToString(secretKey.getEncoded());
+    }
+
+    // Base64 문자열을 SecretKey로 변환
+    public static SecretKey decodeKey(String encodedKey) {
+        byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
+        return new SecretKeySpec(decodedKey, 0, decodedKey.length, ALGORITHM);
+    }
+
+    // 암호화
+    public static String encrypt(String data, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedData);
+    }
+
+    // 복호화
+    public static String decrypt(String encryptedData, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedData = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
+        return new String(decryptedData);
+    }
+
+}
+
