e6c2433bb09f43d94ae72d6b9e02fac7184ff789
juns0720 <juns0720@naver.com>
2025-01-26 10:04:46 +0900

    feat: implement FindTeamInfoByYearDTO

---
+++ src/main/java/com/ghostHoliday/graduationExhibitions/dto/FindTeamInfoByYearDTO.java
@@ -0,0 +1,16 @@
+package com.ghostHoliday.graduationExhibitions.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FindTeamInfoByYearDTO {
+    private int year;
+    private ArrayList<TeamInfoDTO> teams;
+    private ArrayList<ProfessorInfoDTO> professor;
+}
