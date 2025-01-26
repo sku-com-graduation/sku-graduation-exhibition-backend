c2ff3d3212211bf3b253b16b887f346aa9ed84b8
junseo Lee <juns0720@naver.com>
2025-01-26 22:09:41 +0900

    feat: refactor post Entity add post unique UUID

--- src/main/java/com/ghostHoliday/graduationExhibitions/service/PostService.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/service/PostService.java
@@ -1,14 +1,26 @@
 package com.ghostHoliday.graduationExhibitions.service;
 
+import com.ghostHoliday.graduationExhibitions.domain.Account;
 import com.ghostHoliday.graduationExhibitions.domain.Post;
 import com.ghostHoliday.graduationExhibitions.domain.Team;
 import com.ghostHoliday.graduationExhibitions.dto.UpdatePostByTeamDTO;
 import com.ghostHoliday.graduationExhibitions.dto.UpdateSlideImageDTO;
+import com.ghostHoliday.graduationExhibitions.repository.AccountRepository;
 import com.ghostHoliday.graduationExhibitions.repository.PostRepository;
 import com.ghostHoliday.graduationExhibitions.repository.TeamRepository;
+import com.ghostHoliday.graduationExhibitions.utility.AESUtil;
+import com.ghostHoliday.graduationExhibitions.utility.JwtUtility;
+import io.jsonwebtoken.Jwt;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -16,25 +28,33 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostService {
     private final PostRepository postRepository;
     private final TeamRepository teamRepository;
+    private final AccountRepository accountRepository;
+    private final EncryptionService encryptionService;
+    private final JwtUtility jwtUtility;
+
 
     public Long save(Post post) {
         return postRepository.save(post).getId();
     }
 
-//    public void updatePostByTeam(UpdatePostByTeamDTO dto) {
+//    @Transactional
+//    public void updateSlideImage(UpdateSlideImageDTO dto, List<MultipartFile> files) throws Exception {
 //        String token = dto.getToken();
+//        Long teamId = accountRepository.findAccountByUserEmail(jwtUtility.validateToken(token)
+//                .getSubject())
+//                .get()
+//                .getTeam()
+//                .getId();
 //
-//        Long teamId = 1l; // 토큰 암호화 해독 코드 필요
-//        Post post = findPostByTeam(teamId);
-//        Team team = teamRepository.findById(teamId).get();
-//        post.setTitle(dto.getTitle());
-//        post.setContent(dto.getContent());
-//        post.setTeamProfileUrl(dto.getTeamProfileUrl());
-//        post.setDemoUrl(dto.getDemoUrl());
+//        if (!Objects.equals(teamId, encryptionService.decryptPrimaryKey(dto.getEncryptionTeamId()))){
+//            throw new IllegalArgumentException("잘못된 처리입니다.");
+//        }
 //
-//        team.setCategory(dto.getCategory());
+//        String UploadDir = "";
+//        File directory = new File()
 //
-//        postRepository.save(post);
-//        teamRepository.save(team);
+//
 //    }
+
+
 }
