a87d8685f626a33721dd6d83f7e0a9637ebdbf45
juns0720 <juns0720@naver.com>
2025-01-26 11:54:38 +0900

    wip: implement findTeamInfoByYear

--- src/main/java/com/ghostHoliday/graduationExhibitions/service/TeamService.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/service/TeamService.java
@@ -4,6 +4,7 @@ import com.ghostHoliday.graduationExhibitions.domain.Category;
 import com.ghostHoliday.graduationExhibitions.domain.Post;
 import com.ghostHoliday.graduationExhibitions.domain.Team;
 import com.ghostHoliday.graduationExhibitions.dto.FindPostInfoByYearDTO;
+import com.ghostHoliday.graduationExhibitions.dto.FindTeamInfoByYearDTO;
 import com.ghostHoliday.graduationExhibitions.dto.ResponseTeamInfoDTO;
 import com.ghostHoliday.graduationExhibitions.dto.UpdateTeamInfoDTO;
 import com.ghostHoliday.graduationExhibitions.repository.AccountRepository;
@@ -79,6 +80,14 @@ public class TeamService {
         }
         return teams;
     }
+
+//    public List<FindTeamInfoByYearDTO> findTeamInfoByYear(int year, String token) throws Exception {
+//        ArrayList<FindTeamInfoByYearDTO> findTeamInfoByYearDTOS = new ArrayList<>();
+//
+//    }
+
+
+
 
     static ResponseTeamInfoDTO updateTeam(Team team, Long teamId, Long professorId, String name, int year, Category category){
         ResponseTeamInfoDTO teamInfo = new ResponseTeamInfoDTO();
