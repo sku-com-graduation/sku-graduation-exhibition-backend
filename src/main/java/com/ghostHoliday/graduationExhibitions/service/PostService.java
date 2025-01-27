66f7b0f08eb874f757b220a9c51f2c12128cd5a1
junseo Lee <juns0720@naver.com>
2025-01-27 12:36:10 +0900

    feat: implement PostService implement updatePostInfo

--- src/main/java/com/ghostHoliday/graduationExhibitions/service/PostService.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/service/PostService.java
@@ -3,19 +3,12 @@ package com.ghostHoliday.graduationExhibitions.service;
 import com.ghostHoliday.graduationExhibitions.domain.Account;
 import com.ghostHoliday.graduationExhibitions.domain.Post;
 import com.ghostHoliday.graduationExhibitions.domain.Team;
-import com.ghostHoliday.graduationExhibitions.dto.UpdatePostByTeamDTO;
-import com.ghostHoliday.graduationExhibitions.dto.UpdateSlideImageDTO;
 import com.ghostHoliday.graduationExhibitions.repository.AccountRepository;
 import com.ghostHoliday.graduationExhibitions.repository.PostRepository;
 import com.ghostHoliday.graduationExhibitions.repository.TeamRepository;
-import com.ghostHoliday.graduationExhibitions.utility.AESUtil;
 import com.ghostHoliday.graduationExhibitions.utility.FileUtility;
 import com.ghostHoliday.graduationExhibitions.utility.JwtUtility;
-import io.jsonwebtoken.Jwt;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,7 +18,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -48,6 +40,68 @@ public class PostService {
     public Long save(Post post) {
         return postRepository.save(post).getId();
     }
+
+    @Transactional
+    public void updatePostInfo(String token, String encryptionTeamId, MultipartFile teamProfileImg, MultipartFile demo, MultipartFile poster) throws Exception {
+        Account account = accountService.tokenToAccount(token);
+        Team team = account.getTeam();
+        Long decryptedTeamId = encryptionService.decryptPrimaryKey(encryptionTeamId);
+
+        if (!Objects.equals(team.getId(), decryptedTeamId)) {
+            throw new IllegalArgumentException("잘못된 처리입니다.");
+        }
+        Post post = team.getPost();
+
+        // 팀 프로필 이미지 업로드
+        String teamProfileExtension = fileUtility.getImageFileExtension(teamProfileImg.getOriginalFilename());
+        if (teamProfileExtension == null) {
+            throw new IllegalStateException("jpg, png 파일만 업로드 가능합니다. " + teamProfileImg.getOriginalFilename());
+        }
+
+        String teamProfileFileName = "." + teamProfileExtension;
+        Path teamProfileFilePath = Paths.get(post.getTeamProfileUrl() + teamProfileFileName);
+
+        // 기존 파일 삭제
+        if (Files.exists(teamProfileFilePath)) {
+            Files.delete(teamProfileFilePath);
+        }
+
+        Files.write(teamProfileFilePath, teamProfileImg.getBytes());
+
+        // 데모 영상 업로드
+        String demoExtension = fileUtility.getVideoFileExtension(demo.getOriginalFilename());
+        if (demoExtension == null) {
+            throw new IllegalStateException("avi, mp4, mkv 파일만 업로드 가능합니다. " + demo.getOriginalFilename());
+        }
+
+        String demoFileName = "." + demoExtension;
+        Path demoFilePath = Paths.get(post.getDemoUrl() + demoFileName);
+
+        // 기존 파일 삭제
+        if (Files.exists(demoFilePath)) {
+            Files.delete(demoFilePath);
+        }
+
+        Files.write(demoFilePath, demo.getBytes());
+
+        // 포스터 이미지 업로드
+        String posterExtension = fileUtility.getImageFileExtension(poster.getOriginalFilename());
+        if (posterExtension == null) {
+            throw new IllegalStateException("jpg, png 파일만 업로드 가능합니다. " + poster.getOriginalFilename());
+        }
+
+        String posterFileName = "." + posterExtension;
+        Path posterFilePath = Paths.get(post.getPosterUrl() + posterFileName);
+
+        // 기존 파일 삭제
+        if (Files.exists(posterFilePath)) {
+            Files.delete(posterFilePath);
+        }
+
+        Files.write(posterFilePath, poster.getBytes());
+    }
+
+
 
     @Transactional
     public void updateSlideImage(String token,String encryptionTeamId, List<MultipartFile> files) throws Exception {
@@ -76,7 +130,7 @@ public class PostService {
 
         int fileIndex = 1;
         for (MultipartFile file : files) {
-            String extention = fileUtility.getFileExtension(file.getOriginalFilename());
+            String extention = fileUtility.getImageFileExtension(file.getOriginalFilename());
             // 이미지 파일 여부 확인
             if (extention == null) {
                 throw new IllegalStateException("jpg, png 파일만 업로드 가능합니다. " + file.getOriginalFilename());
@@ -93,10 +147,12 @@ public class PostService {
             throw new RuntimeException("파일 업로드 중 오류 발생: " + e.getMessage(), e);
         }
     }
+
     static void cleanDirectory(File directory) {
         File[] files = directory.listFiles();
         Arrays.stream(files).forEach(File::delete);
     }
+
 
 }
 
--- src/main/java/com/ghostHoliday/graduationExhibitions/utility/FileUtility.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/utility/FileUtility.java
@@ -1,21 +1,26 @@
 package com.ghostHoliday.graduationExhibitions.utility;
 
 
-import com.ghostHoliday.graduationExhibitions.domain.Team;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.nio.file.Files;
 
 
 @Service
 public class FileUtility {
     
     // 사진 확장자 추출기
-    public String getFileExtension(String fileName) {
+    public String getImageFileExtension(String fileName) {
         // 파일 이름에서 확장자 추출
         String extension = null;
-        if (fileName != null && (fileName.endsWith(".jpg") || fileName.endsWith(".png"))) {
+        if (fileName != null && (fileName.endsWith(".jpg") || fileName.endsWith(".png") )) {
+            extension = fileName.substring(fileName.lastIndexOf('.') + 1);
+        }
+        return extension;
+    }
+
+    public String getVideoFileExtension(String fileName) {
+        // 파일 이름에서 확장자 추출
+        String extension = null;
+        if (fileName != null && (fileName.endsWith(".mkv") || fileName.endsWith(".mp4") || fileName.endsWith(".avi") )) {
             extension = fileName.substring(fileName.lastIndexOf('.') + 1);
         }
         return extension;
