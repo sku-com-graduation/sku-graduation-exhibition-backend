d805e79642f891903e311a89a0f9f4cd71580d73
junseo Lee <juns0720@naver.com>
2025-02-03 00:29:09 +0900

    feat: Implement DataInitializer

--- src/main/java/com/ghostHoliday/graduationExhibitions/repository/AccountRepository.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/repository/AccountRepository.java
@@ -19,6 +19,5 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
     void deleteByTeamId(Long teamId);
 
-    Optional<Account> findAccountByuserEmailAndPwd(String userEmail, String password);
 
 }
