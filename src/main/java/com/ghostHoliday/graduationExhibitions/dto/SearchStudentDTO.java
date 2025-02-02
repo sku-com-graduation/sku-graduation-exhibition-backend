f0cab1677f4687e389c02585a4171c7889198fb1
junseo Lee <juns0720@naver.com>
2025-02-03 01:07:35 +0900

    fix: encrypt primaryKey of Student

--- src/main/java/com/ghostHoliday/graduationExhibitions/dto/SearchStudentDTO.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/dto/SearchStudentDTO.java
@@ -8,8 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SearchStudentDTO{
-    private Long studentId;
-    private Long teamId;
+    private String encryptedStudentId;
+    private String encryptedTeamId;
     private String name;
     private String studentNumber;
     private String role;
