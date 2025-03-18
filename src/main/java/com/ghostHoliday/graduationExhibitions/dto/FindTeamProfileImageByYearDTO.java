676b21e2988bd3fdbff9d6eafc143f453eaf6261
junseo Lee <juns0720@naver.com>
2025-03-19 00:02:43 +0900

    feat: 연도별 팀 프로필 이미지 조회 기능 구현

---
+++ src/main/java/com/ghostHoliday/graduationExhibitions/dto/FindTeamProfileImageByYearDTO.java
@@ -0,0 +1,12 @@
+package com.ghostHoliday.graduationExhibitions.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FindTeamProfileImageByYearDTO {
+    private String teamProfileImage;
+}
