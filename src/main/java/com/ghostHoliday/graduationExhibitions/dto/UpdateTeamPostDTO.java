4aceb9d39c704b2ab5d43929b448ad8f04fa9e51
Yithian01 <dbwlsdud10@naver.com>
2025-03-18 20:51:32 +0900

    refactor: updateTeamPost-logic change UpdateTeamPostDTO

--- src/main/java/com/ghostHoliday/graduationExhibitions/dto/UpdateTeamPostDTO.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/dto/UpdateTeamPostDTO.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateTeamPostDTO {
-    private String encryptedTeamId;
+    private String teamUuId;
     private String title;
     private String content;
     private Category category;
