ae10ba641b81d3531f270528f375c207feb8e1ef
junseo Lee <juns0720@naver.com>
2025-03-20 00:25:41 +0900

    fix: 팀 수정 페이지 권한 검증 추가

---
+++ src/main/java/com/ghostHoliday/graduationExhibitions/dto/EditStudentInfoResponseDTO.java
@@ -0,0 +1,20 @@
+package com.ghostHoliday.graduationExhibitions.dto;
+
+import com.ghostHoliday.graduationExhibitions.domain.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EditStudentInfoResponseDTO {
+    private String encryptedStudentProfileId;
+    private String name;
+    private String info;
+    private Role role;
+    private String githubUrl;
+    private String studentEmail;
+    private String studentBlog;
+    private String profileImage;
+}
