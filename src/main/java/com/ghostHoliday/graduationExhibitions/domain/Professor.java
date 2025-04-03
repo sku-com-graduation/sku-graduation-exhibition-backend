f0ddd5a6d875a2a1d7889f76eac0d2aee276d043
juns0720 <juns0720@naver.com>
2025-04-03 16:53:36 +0900

    fix: 같은 교수를 가진 팀이 다수가 존재하지 못하는 오류 수정

--- src/main/java/com/ghostHoliday/graduationExhibitions/domain/Professor.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/domain/Professor.java
@@ -1,6 +1,7 @@
 package com.ghostHoliday.graduationExhibitions.domain;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -11,6 +12,7 @@ import lombok.Data;
 public class Professor {
 
     @Id @GeneratedValue
+    @Column(name = "professor_id")
     private Long id;
 
     private String name;
