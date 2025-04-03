9afa7e956229733021bbce67b88e133b50643103
juns0720 <juns0720@naver.com>
2025-04-03 18:37:42 +0900

    fix: 학생 정보 저장 로직 수정 데이터를 받아서 한 번에 저장 가능하도록 수정

--- src/main/java/com/ghostHoliday/graduationExhibitions/controller/StudentController.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/controller/StudentController.java
@@ -30,19 +30,14 @@ public class StudentController {
     private final EncryptionService encryptionService;
     private final HttpOnlyService httpOnlyService;
 
-
-
-
     // CSV 파일을 받아서 처리하는 메소드
     @PostMapping("/save")
     @PreAuthorize("hasAnyAuthority('ADMIN')")
     public ResponseEntity<String> saveStudent(
             HttpServletRequest request,
             HttpServletResponse response,
-            @RequestBody SaveStudentDTO dto) {
-
+            @RequestBody List<SaveStudentDTO> dto) {
         try {
-
                 studentService.saveStudent(dto);
 
             // 저장 완료 메시지 반환
@@ -55,39 +50,6 @@ public class StudentController {
         }
     }
 
-
-    // CSV 파일을 받아서 처리하는 메소드
-    @PostMapping("s/save")
-    @PreAuthorize("hasAnyAuthority('ADMIN')")
-    public ResponseEntity<String> saveStudents(
-            HttpServletRequest request,
-            HttpServletResponse response,  // accessToken 재발급을 위해 추가
-            @RequestParam("file") MultipartFile file) {
-        try {
-
-
-            // CSV 파일을 OpenCSV로 읽음
-            try (CSVReader csvReader = new CSVReader(new InputStreamReader(file.getInputStream() , StandardCharsets.UTF_8))) {
-                List<String[]> rows = csvReader.readAll(); // 파일의 모든 내용을 읽어옴
-
-                // CSV 파일에서 각 행을 SaveStudentDTO로 변환
-                List<SaveStudentDTO> dto = rows.stream()
-                        .map(row -> new SaveStudentDTO(row[0], row[1])) // 각 행을 SaveStudentDTO로 변환
-                        .collect(Collectors.toList());
-
-                // 변환된 DTO 객체들을 데이터베이스에 저장
-                studentService.saveStudents(dto);
-            }
-
-            // 저장 완료 메시지 반환
-            return ResponseEntity.status(HttpStatus.CREATED)
-                    .body("학생 정보가 성공적으로 저장되었습니다.");
-        }  catch (Exception e) {
-            // 예외 발생 시 실패 메시지 반환
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("학생 정보 저장 중 오류가 발생했습니다. 파일 형식 또는 내용 확인을 해주세요.");
-        }
-    }
     @PreAuthorize("hasAnyAuthority('ADMIN')")
     @DeleteMapping("/delete")
     public ResponseEntity<String> deleteStudents(
