5726a88bc958e4a83c53ff1cc5c4b579b4e6b700
junseo Lee <juns0720@naver.com>
2025-05-15 00:45:36 +0900

    feat: 프론트 기반 학생 프로필 정보 업데이트 기능 구현

--- src/main/java/com/ghostHoliday/graduationExhibitions/controller/S3UploadController.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/controller/S3UploadController.java
@@ -2,9 +2,11 @@ package com.ghostHoliday.graduationExhibitions.controller;
 
 import com.ghostHoliday.graduationExhibitions.dto.post.UpdatePostInfoV2Request;
 import com.ghostHoliday.graduationExhibitions.dto.post.S3UrlDTO;
-import com.ghostHoliday.graduationExhibitions.dto.post.fileInfoDTO;
+import com.ghostHoliday.graduationExhibitions.dto.post.FileInfoDTO;
+import com.ghostHoliday.graduationExhibitions.dto.studentProfile.UpdateStudentInfoV2Request;
 import com.ghostHoliday.graduationExhibitions.service.PostService;
 import com.ghostHoliday.graduationExhibitions.service.ProfessorService;
+import com.ghostHoliday.graduationExhibitions.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,7 @@ public class S3UploadController {
 
     private final PostService postService;
     private final ProfessorService professorService;
+    private final StudentService studentService;
 
     @PostMapping("/presigned-url/post")
     public ResponseEntity<List<S3UrlDTO>> getPreSignedUrlByPost(
@@ -30,10 +33,22 @@ public class S3UploadController {
 
     @PostMapping("/presigned-url/professor")
     public ResponseEntity<S3UrlDTO> getPreSignedUrlByProfessor(
-            @RequestBody fileInfoDTO request
+            @RequestBody FileInfoDTO request
     ) {
         S3UrlDTO response = professorService.updateProfessorInfoV2(request);
 
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/presigned-url/student")
+    public ResponseEntity<S3UrlDTO> getPreSignedUrlByStudent(
+            @RequestBody UpdateStudentInfoV2Request request
+    ) throws Exception {
+        S3UrlDTO response = studentService.updateStudentInfoV2(request);
+
+        return ResponseEntity.ok(response);
+    }
+
+
+
 }
