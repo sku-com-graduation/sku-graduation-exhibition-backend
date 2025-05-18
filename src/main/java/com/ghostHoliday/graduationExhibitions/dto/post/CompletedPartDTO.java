9edfc8f2c32903719bfc2450b318e141fb8f635f
juns0720 <juns0720@naver.com>
2025-05-18 15:40:45 +0900

    refactor: eTag Json에서 명시

--- src/main/java/com/ghostHoliday/graduationExhibitions/dto/post/CompletedPartDTO.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/dto/post/CompletedPartDTO.java
@@ -1,9 +1,11 @@
 package com.ghostHoliday.graduationExhibitions.dto.post;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class CompletedPartDTO {
     private int partNumber;
-    private String e_Tag;
+    @JsonProperty("eTag")
+    private String eTag;
 }
