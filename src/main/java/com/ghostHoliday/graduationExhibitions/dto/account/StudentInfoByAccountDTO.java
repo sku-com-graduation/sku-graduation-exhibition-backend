aaee6291c36714cf0227469def6f14aa16e5d781
juns0720 <juns0720@naver.com>
2025-09-27 03:35:30 +0900

    feat: add exception

--- src/main/java/com/ghostHoliday/graduationExhibitions/dto/account/StudentInfoByAccountDTO.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/dto/account/StudentInfoByAccountDTO.java
@@ -1,8 +1,11 @@
 package com.ghostHoliday.graduationExhibitions.dto.account;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Value;
+import software.amazon.awssdk.annotations.NotNull;
 
 @Data
 @AllArgsConstructor
@@ -10,5 +13,6 @@ import lombok.NoArgsConstructor;
 public class StudentInfoByAccountDTO {
     private String studentName;
     private String studentNumber;
+    @NotNull @NotBlank
     private String studentEmail;
 }
