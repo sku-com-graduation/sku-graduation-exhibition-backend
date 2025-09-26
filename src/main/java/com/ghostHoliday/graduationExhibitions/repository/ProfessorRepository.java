018739905e06735739d08dc2519fab3edddc8e7a
juns0720 <juns0720@naver.com>
2025-09-27 04:25:44 +0900

    feat: update professor info

--- src/main/java/com/ghostHoliday/graduationExhibitions/repository/ProfessorRepository.java
+++ src/main/java/com/ghostHoliday/graduationExhibitions/repository/ProfessorRepository.java
@@ -3,6 +3,9 @@ package com.ghostHoliday.graduationExhibitions.repository;
 import com.ghostHoliday.graduationExhibitions.domain.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
 
+    Optional<Professor> findByName(String professorName);
 }
